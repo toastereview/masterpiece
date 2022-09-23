a4fe522e00a193ea1e9ec6954f11ddf7e91a924b
toastereview <114227566+toastereview@users.noreply.github.com>
2022-09-23 17:05:13 +0200

    improve

--- main.py
+++ main.py
@@ -24,41 +24,12 @@ def test_line(line):
 #         if e in l:
 #             n=n+1
 #     return n
-def process(l):
-    n = 0
-    for i in range(len(l)):
-        if l[i] == "f":
-            if i+1 < len(l):
-                if l[i+1] == 'o':
-                    if i+2 < len(l):
-                        if l[i+2] == 'o':
-                            n+=1
-    return n
-
-def compute_stats(file_path):
-    stats = {}
-    with open(file_path) as f:
-        for line in f:
-            stats["lines"] = stats["lines"] + 1 if "lines" in stats else 1
-
-            wc = 0
-            for e in line:
-                wc = wc + 1
-
-            if "foo" in stats:
-                stats["foo"] += process(line)
-            else:
-                stats["foo"] = process(line)
-            try:
-                if not test_line(line):
-                    stats["empty_lines"] += 1
-            except:
-                if not test_line(line):
-                    stats["empty_lines"] = 1
-    return stats
-
 
 from sys import argv
+
+if False:
+    # argv = ["C:\\Users\\Alexandru\\Desktop\\test.txt"]
+    argv = ["C:\\Users\\Alexandru\\Desktop\\Python\\Python\\Lab 2\\main.py"]
 
 if len(argv) < 2:
     print("usage: python3 main.py <FILES>")
@@ -80,7 +51,32 @@ for path in paths:
             extension = path[i + 1 :]
             break
     extension = extension.upper()
-    new_stats = compute_stats(path)
+    stats = {}
+    with open(path) as f:
+        for line in f:
+            stats["lines"] = stats["lines"] + 1 if "lines" in stats else 1
+
+            wc = 0
+            for e in line:
+                wc = wc + 1
+
+            c = 0
+            for i in range(len(line)):
+                if i + 2 < len(line):
+                    if line[i:i+3] == "foo":
+                        c+=1
+
+            if "foo" in stats:
+                stats["foo"] += c
+            else:
+                stats["foo"] = c
+
+            if not test_line(line):
+                if "empty_lines" in stats:
+                    stats["empty_lines"] += 1
+                else:
+                    stats["empty_lines"] = 1
+    new_stats = stats
     old_stats = global_stats[extension] if extension in global_stats else {}
     global_stats[extension] = merge_stats([old_stats, new_stats])
 
@@ -91,9 +87,3 @@ for ext in global_stats:
     for key, value in v.items():
         print("  " + key + ":" + " " + str(value))
     print()
-
-
-if False:
-    # stats = compute_stats("C:\\Users\\Alexandru\\Desktop\\test.txt")
-    stats = compute_stats("C:\\Users\\Alexandru\\Desktop\\Python\\Python\\Lab 2\\main.py")
-    print(stats)
